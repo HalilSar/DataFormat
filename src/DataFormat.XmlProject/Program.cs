aca7cc875c4b9f513279606b66fa43d71366d8d2
halil37 <hllibrahimsartitas@gmail.com>
2024-10-19 23:18:30 +0300

    xml-write method created.

--- src/DataFormat.XmlProject/Program.cs
+++ src/DataFormat.XmlProject/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Xml;
 namespace DataFormat.XmlProject
 {
     class Program
@@ -7,6 +7,29 @@ namespace DataFormat.XmlProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            WriteXmlToDoc(new Personal { Id = 1, Name = "Martin", SurName = "Fowler" }, @"D:\dotnet\.NetProject\Net53.DataExchangeFormat\src\DataFormat.XmlProject\Document2.xml");
+
+
+        }
+
+
+        static void WriteXmlToDoc(Personal personal, string path)
+        {
+            //Xmlt
+            
+            var xml = XmlWriter.Create(path);
+            xml.WriteStartDocument(true);
+
+            xml.WriteStartElement("personal");
+            xml.WriteAttributeString("Id", personal.Id.ToString());
+         
+            xml.WriteElementString("Ad",personal.Name);
+            xml.WriteElementString("Soyad", personal.SurName);
+      
+            xml.WriteEndElement();
+
+            xml.Close();
         }
     }
 }
