d94bf5e8c1c03c2eb0edae61a8e6092b90d630a7
halil37 <hllibrahimsartitas@gmail.com>
2024-10-25 23:37:52 +0300

    Personal class created.

--- src/DataFormat.JsonProject/Program.cs
+++ src/DataFormat.JsonProject/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using Newtonsoft.Json;
+using System.IO;
 namespace DataFormat.JsonProject
 {
+    public class Personal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+
+        static void WriteJson(string value,string path)
+        {
+          //  var 
         }
     }
 }
