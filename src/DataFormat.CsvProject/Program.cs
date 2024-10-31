553c5727e762c6fb0d6062f6417761771c55dc51
halil37 <hllibrahimsartitas@gmail.com>
2024-10-31 19:29:17 +0300

    Personal class added.

--- src/DataFormat.CsvProject/Program.cs
+++ src/DataFormat.CsvProject/Program.cs
@@ -2,6 +2,12 @@
 
 namespace DataFormat.CsvProject
 {
+    public class Personal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
