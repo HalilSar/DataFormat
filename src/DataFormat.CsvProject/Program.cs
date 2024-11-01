526255d1f148d60ee861feea264f11c42623fd41
halil37 <hllibrahimsartitas@gmail.com>
2024-11-01 21:52:48 +0300

    programcs modified.

--- src/DataFormat.CsvProject/Program.cs
+++ src/DataFormat.CsvProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using static System.Console;
 using CsvHelper;
+using System.Collections.Generic;
 using System.IO;
 namespace DataFormat.CsvProject
 {
@@ -14,17 +15,20 @@ namespace DataFormat.CsvProject
     {
         static void Main(string[] args)
         {
-            string path = @".\your\path\data2.csv";          
-            WriteCsv( path,new Personal { Id = 1, Name = "Martin", SurName = "Fowler" });
+            string path = @".\your\path\data2.csv";
+            WriteCsv(path, new Personal { Id = 1, Name = "Martin", SurName = "Fowler" });
+
         }
 
         static void WriteCsv(string path, Personal personal)
         {
            
             StreamWriter sw = new StreamWriter(path);
-            CsvWriter csvWriter = new CsvWriter(sw,System.Globalization.CultureInfo.InvariantCulture);
-            csvWriter.WriteHeader(typeof(Personal));
-            csvWriter.WriteRecord(personal);
+            using (CsvWriter csvWriter = new CsvWriter(sw, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteHeader<Personal>();
+                csvWriter.WriteRecord(personal);
+            }
             sw.Close();
         }
 
