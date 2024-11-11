78b00f9696b810a7195227000771297d4a6dc597
halil37 <hllibrahimsartitas@gmail.com>
2024-11-11 20:48:08 +0300

    comment added for documantion at programcs.

--- src/DataFormat.XmlProject/Program.cs
+++ src/DataFormat.XmlProject/Program.cs
@@ -7,7 +7,7 @@ namespace DataFormat.XmlProject
     {
         static void Main(string[] args)
         {
-            string path = @".\your\path\demo.xml";
+            string path =  @".\your\path\demo.xml";
             string path2 = @".\your\path\demo2.xml";
 
             WriteXmlToDoc(new Personal { Id = 1, Name = "Martin", SurName = "Fowler" }, path);
@@ -15,38 +15,35 @@ namespace DataFormat.XmlProject
             WriteXmlToDoc(new List<Personal> { new Personal { Id = 1, Name = "Martin", SurName = "Fowler" },
                                                new Personal { Id = 2, Name = "Goerge", SurName = "Bool" },
                                                new Personal { Id = 3, Name = "Goerge", SurName = "Leibniz" }}, path2);
-            ReadXml(path);
-            Console.WriteLine("******************************");
             ReadXml(path2);
+            Console.WriteLine("******************************");
+            //ReadXml(path2);
 
         }
 
-
+        // Write only  personal object in the xml file
+        // parameters: personal object and string path
         static void WriteXmlToDoc(Personal personal, string path)
         {
-            //Xmlt
-            
+
+
             var xml = XmlWriter.Create(path);
-            xml.WriteStartDocument(true);
-
-            xml.WriteStartElement("personal");
-            xml.WriteAttributeString("Id", personal.Id.ToString());
-            
-            xml.WriteElementString("Ad",personal.Name);
-            
-
-            xml.WriteElementString("Soyad", personal.SurName);
            
-
+           
+            xml.WriteStartElement("personal");
+            xml.WriteAttributeString("Id", personal.Id.ToString());           
+            xml.WriteElementString("Ad",personal.Name);
+            xml.WriteElementString("Soyad", personal.SurName);           
             xml.WriteEndElement();
 
             xml.Close();
         }
-    
+
+        // Write personal list in the xml fil
+        // parameters: personal list and string path
         static void WriteXmlToDoc(List<Personal> persons, string path)
         {
             var xml  = XmlWriter.Create(path);
-            xml.WriteStartDocument(true);
             xml.WriteStartElement("Personeller");
             
             foreach (var personal in persons)
@@ -67,6 +64,8 @@ namespace DataFormat.XmlProject
             xml.Close();
         }
 
+        // Read all personal in the xml file
+        // parameters: string path
         static void ReadXml(string path)
         {
             var xmlreader = XmlReader.Create(path);
